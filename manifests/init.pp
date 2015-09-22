54d1e341b446c6c7e9de043d08899118731f5ffc
Sebastian Marcet <smarcet@gmail.com>
2015-09-22 12:36:53 -0300

    Added bower support
    added bower installation procedure to remove bower hardcoded
    dependencies on https://review.openstack.org/#/c/198458/
    
    Change-Id: Ic0a9b3a2dcfd84df35490fcd20c8399b3e0fd3cc

--- manifests/init.pp
+++ manifests/init.pp
@@ -74,6 +74,24 @@ class openstackid (
     ensure => present,
   }
 
+  # add node js and npm for bower installation
+  package { 'nodejs':
+    ensure => present,
+  }
+
+  package { 'npm':
+    ensure  => present,
+    require => Package['nodejs'],
+  }
+
+  exec { 'install-bower':
+    cwd       => '/',
+    path      => '/usr/bin:/bin:/usr/local/bin:/usr/lib/node_modules/npm/bin',
+    logoutput => on_failure,
+    command   => 'npm install -g bower',
+    require   => Package['npm','nodejs'],
+  }
+
   group { 'openstackid':
     ensure => present,
   }
@@ -245,7 +263,7 @@ class openstackid (
       File['/etc/openstackid/environment.php'],
       File['/etc/openstackid/server.php'],
       File['/etc/openstackid/app.php'],
-      Package['curl'],
+      Package['curl','nodejs','npm'],
       Package[$php5_packages] ],
   }
 
@@ -263,6 +281,7 @@ class openstackid (
       File['/etc/openstackid/log.php'],
       File['/etc/openstackid/environment.php'],
       File['/etc/openstackid/server.php'],
+      Package['nodejs','npm'],
       Package[$php5_packages] ],
   }
 
